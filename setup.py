082df64a4fe602dc34e9e89a39b3612e4f2b1687
Stan Hu <stanhu@aclimalabs.com>
2013-05-24 12:55:36 -0700

    Reorganize and add setup.py to support pip installs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='dowser',
+    version='0.1',
+    packages=['dowser',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
