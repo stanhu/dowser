f72df6d95a39ca0585939fd701f90b30af509d80
Stan Hu <stanhu@aclimalabs.com>
2013-06-06 14:41:02 -0700

    Add HTML/CSS files to build

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(
     packages=['dowser',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    package_dir={'dowser': 'dowser'},
+    package_data=dict(dowser=['*.html', 'main.css'])
 )
